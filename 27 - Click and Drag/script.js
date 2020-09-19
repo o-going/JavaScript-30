88fc62546b76585a55798a57a83dd7cbc6a96165
ga young <65806496+o-going@users.noreply.github.com>
2020-09-20 01:37:07 +0900

    27 - Click and Drag

---
+++ 27 - Click and Drag/script.js
@@ -0,0 +1,32 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  console.log(e.pageX);
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  console.log(startX);
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  console.log({x, startX});
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+});
